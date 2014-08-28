9458a0896828b4b0335d61c0eaeb696853bdeea6
Miguel Angel Martinez Zamora <natacion@gmail.com>
2014-08-28 18:56:27 -0500

    first commit

---
+++ old/dc.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Acceso Restringido</title>
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+.contenido {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	font-weight: bolder;
+	color: #666666;
+	text-decoration: none;
+	text-align: center;
+}
+-->
+</style>
+</head>
+
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td background="imagenes/top_a3.gif"><table width="100" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><img src="imagenes/top_a1.gif" width="129" height="55" /></td>
+        <td><img src="imagenes/top_a2.gif" width="170" height="55" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center"><table width="519" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="519" height="45" align="center" valign="top"><img src="imagenes/bloque_Top.gif" width="476" height="33" /></td>
+      </tr>
+      <tr>
+        <td class="contenido">Las pol&iacute;ticas implementadas en la UCI; para el uso del Internet,
+          NO permiten mostrar el contenido parcial o total del sitio WEB solicitado.<br />
+          <br />
+          Si el contenido del sitio WEB es indispensable para el logro de tus actividades,
+          selecciona el bot&oacute;n de Solicitar Activaci&oacute;n y llena el formulario completo.</td>
+      </tr>
+      <tr>
+        <td height="35" align="center" valign="bottom"><img src="imagenes/bloque_Down.gif" width="476" height="24" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="55" align="center" valign="top"><a href="Solicitud.html" target="_self"><img src="imagenes/botSolicitar.gif" width="115" height="27" vspace="5" border="0" /></a></td>
+  </tr>
+  <tr>
+    <td align="center"><img src="imagenes/logo.png"></td>
+  </tr>
+</table>
+</body>
+</html>
